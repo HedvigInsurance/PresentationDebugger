12918919abb58ce94b59e2a7c094ba17416b4831
Sam Pettersson <sam@sampettersson.com>
2021-07-18 17:35:59 +0200

    Initial Commit

---
+++ Shared/PresentationDebuggerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PresentationDebuggerApp.swift
+//  Shared
+//
+//  Created by Sam Pettersson on 2021-07-18.
+//
+
+import SwiftUI
+
+@main
+struct PresentationDebuggerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
